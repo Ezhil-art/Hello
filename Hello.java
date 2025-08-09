91a769de58027dbc23782b1efb5f82cbaa9d0559
Ezhil-art <Ezhilaadh@gmail.com>
2025-08-09 21:19:27 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -5,8 +5,10 @@ public static void main(String[] args) {
 System.out.println("Ezhilann");
   System.out.println("kaadhwi");
   System.out.println("Aadhvik");
+  System.out.println("nan");
 }
 }
+
 
 
 
